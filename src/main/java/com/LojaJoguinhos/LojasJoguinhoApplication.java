0fb6ea8b2ea32f3c83ec264365a11c78481a78a2
lucas <dantaslucas337@gmail.com>
2022-08-05 09:06:49 -0300

    Inicio do projeto da atividade 05

---
+++ src/main/java/com/LojaJoguinhos/LojasJoguinhoApplication.java
@@ -0,0 +1,13 @@
+package com.LojaJoguinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojasJoguinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojasJoguinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LojaJoguinhos/LojasJoguinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LojaJoguinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojasJoguinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
